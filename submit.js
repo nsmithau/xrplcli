22db7f9da600b11e0835e50ba4645b3860198823
Mwni <manuel@tigmar.de>
2024-10-31 14:54:35 +0100

    simplify tx submission

--- submit.js
+++ submit.js
@@ -1,10 +1,6 @@
 import { connect } from './net.js'
-import { askPayload } from './terminal.js'
 
 export async function submit({ payload }){
-	if(!payload)
-		payload = await askPayload({ message: 'enter payload to submit:' })
-
 	let socket = await connect()
 
 	console.log('submitting...')
@@ -15,14 +11,16 @@ export async function submit({ payload }){
 			tx_blob: payload
 		})
 
-		console.log(`${result.engine_result === 'tesSUCCESS' ? '\x1b[32m' : '\x1b[33m'}submitted: ${result.engine_result}\n${result.engine_result_message}\x1b[0m`)
-		console.log(`transaction hash: ${result.tx_json.hash}`)
+		console.log(`${result.engine_result_message} (${result.engine_result})`)
+		console.log(`hash: ${result.tx_json.hash}`)
 		process.exit(0)
 	}catch(error){
 		console.log(`\x1b[31msubmission failed!\x1b[0m`)
 
-		if(error.error)
-			console.error(`reason: ${error.error_message} (code ${error.error_code})`)
+		if(error.error_message)
+			console.error(`reason: ${error.error_message} (${error.error})`)
+		else if(error.error_exception)
+			console.error(`reason: ${error.error_exception} (${error.error})`)
 		else
 			console.error(error)
 
