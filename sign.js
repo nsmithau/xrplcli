2ee5d85bba717a8eb2b673b0b3323e3f3fb70bc2
Mwni <manuel@tigmar.de>
2024-11-03 18:21:41 +0100

    implement signed tx qr code printing

--- sign.js
+++ sign.js
@@ -1,6 +1,7 @@
 import { sign as libSignTx } from '@xrplkit/submit'
 import { encode } from 'ripple-binary-codec'
-import { askChoice, askJSON, askMnemonic } from './terminal.js'
+import qrcode from 'qrcode-terminal'
+import { ask, askChoice, askJSON, askMnemonic } from './terminal.js'
 import { askSecret } from './wallet.js'
 import { submit } from './submit.js'
 
@@ -68,6 +69,31 @@ async function signTx({ tx }){
 		})
 	
 		switch(nextAction){
+			case 'qr': {
+				let parts = await ask({
+					message: 'split into multiple QR codes (1-4): ',
+					validate: input => !(parseInt(input) >= 1 && parseInt(input) <= 4)
+						&& 'must be between 1 and 4 parts - try again'
+				})
+				let chunk = Math.ceil(signedBlob.length / parseInt(parts))
+				let offset = 0
+				let i = 0
+
+				while(++i){
+					qrcode.generate(signedBlob.slice(offset, offset + chunk), {small: true})
+
+					offset += chunk
+
+					if(offset >= signedBlob.length){
+						console.log(`part ${i} of ${parts}`)
+						break
+					}else{
+						await ask({ message: `part ${i} of ${parts} - press enter for next` })
+					}
+				}
+				break
+			}
+
 			case 'submit': {
 				await submit({ payload: signedBlob })
 				return
