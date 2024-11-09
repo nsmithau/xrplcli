3d01668f54e7526c7164f07f1aae1096339fd4f3
Mwni <manuel@tigmar.de>
2024-11-09 12:14:22 +0100

    use api v2

--- net.js
+++ net.js
@@ -2,16 +2,16 @@ import createSocket from '@xrplkit/socket'
 
 export async function connect({ url = 'wss://s1.ripple.com' } = {}){
 	let cleanUrl = url.replace('ws://', '').replace('wss://', '')
-	let socket = createSocket({ url })
+	let socket = createSocket({ url, apiVersion: 2 })
 
-	console.log(`connecting to ${cleanUrl}...`)
+	process.stdout.write(`connecting to ${cleanUrl}... `)
 
 	await new Promise((resolve, reject) => {
 		socket.on('open', resolve)
 		socket.on('error', reject)
 	})
 
-	console.log('connection established')
+	console.log('established')
 
 	return socket
 }
